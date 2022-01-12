4e6a33955e16e797cc8727546276582acc7aafe6
Marcus Urani <marcus.urani20@gmail.com>
2022-01-12 20:26:57 -0300

    Estilizando as listas de filmes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "./App.css";
 import TMDB from "./Api/tmdb";
 import List from "./Components/List";
 
--- src/Components/List/index.js
+++ src/Components/List/index.js
@@ -1,18 +1,22 @@
 import React from "react";
+import "./style.css";
 
 const List = ({ title, items }) => {
   return (
     <section className="homelist">
       <h1 className="homelist__title">{title}</h1>
       <div className="homelist__listarea">
-        {items.results.length > 0 &&
-          items.results.map((item, id) => (
-            <img
-              key={id}
-              src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
-              alt={item.original_title}
-            />
-          ))}
+        <div className="homelist__list">
+          {items.results.length > 0 &&
+            items.results.map((item, id) => (
+              <figure key={id} className="homelist__item">
+                <img
+                  src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
+                  alt={item.original_title}
+                />
+              </figure>
+            ))}
+        </div>
       </div>
     </section>
   );
