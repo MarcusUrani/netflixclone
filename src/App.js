acb96131042f49e4d23f2b758e088ad4b408a021
Marcus Urani <marcus.urani20@gmail.com>
2022-01-13 19:47:36 -0300

    Criando a lógica da sessão filmes em destaque

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import TMDB from "./Api/tmdb";
 import List from "./Components/List";
+import FeaturedMovie from "./Components/FeaturedMovie";
 
 function App() {
   const [movieList, setMovieList] = useState([]);
-  //Pegando a lista dos filmes
+  const [featuredData, setFeaturedData] = useState(null);
+
+  //Buscando a lista dos filmes
   const loadAll = async () => {
     let list = await TMDB.getHomeList();
     setMovieList(list);
+
+    //Buscando o item em destaque
+    let originals = list.filter((i) => i.slug === "originals");
+    let randomChoice = Math.floor(
+      Math.random() * (originals[0].items.results.length - 1)
+    );
+    let chosen = originals[0].items.results[randomChoice];
+    setFeaturedData(chosen);
   };
 
   useEffect(() => {
@@ -16,6 +27,7 @@ function App() {
   }, []);
   return (
     <section className="page">
+      {featuredData && <FeaturedMovie item={featuredData} />}
       {movieList.map((item, id) => (
         <List key={id} title={item.title} items={item.items} />
       ))}
---
+++ src/Components/FeaturedMovie/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const FeaturedMovie = () => {
+    return (
+        <h1>
+            Filme em destaque!
+        </h1>
+    )
+};
+
+export default FeaturedMovie;
